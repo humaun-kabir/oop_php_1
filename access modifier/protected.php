4cc6befc1689157a1d22e8bc99bcbdf68384ce1a
Humaun Kabir <asmhumaunkabir@gmail.com>
2021-06-11 05:05:40 +0600

    Create protected.php

---
+++ access modifier/protected.php
@@ -0,0 +1,25 @@
+<?php
+
+class Base{
+	protected $name;
+
+	public function __construct($n){
+		$this->name = $n;
+	}
+
+	protected function show(){
+		echo "your name : " . $this->name ."<br>";
+	}
+}
+
+class Derived extends Base{
+	public function get(){
+		echo "your name :" . $this->name . "<br>";
+	}
+}
+
+$test = new Derived("tausif");
+
+//$test->name = "humaun";
+
+$test->get();
